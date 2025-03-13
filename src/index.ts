1474fd13f32cc4b372f8c8141531bbd0ce8e6d37
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2025-03-13 01:56:18 +0100

    latest

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 8080;
 const app = createApp();
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:8081/v1/swift-codes`);
 });
